cf3c1f99c1d6e83e82eca3bcbafa874c36c69405
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-03-28 23:08:16 -0300

    Criação da pasta database e implementação do ScriptBanco.sql com a modelagem das tabelas.

---
+++ database/ScriptBanco.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE projeto_integrador;
+USE projeto_integrador;
+
+CREATE TABLE cadastro_eleitores (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) UNIQUE NOT NULL,
+    titulo_eleitor CHAR(12) UNIQUE NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    mesario BOOLEAN DEFAULT FALSE,
+    chave_acesso CHAR(7) UNIQUE NOT NULL,
+    status_votacao BOOLEAN DEFAULT FALSE NOT NULL
+);
+
+CREATE TABLE cadastro_candidatos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) UNIQUE NOT NULL,
+    partido VARCHAR(50) NOT NULL,
+    sigla_partido VARCHAR(2) NOT NULL,
+    numero_votacao CHAR(2) UNIQUE NOT NULL
+);
+
+CREATE TABLE votos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    id_candidato INT,
+    data_hora DATETIME NOT NULL,
+    protocolo_votacao VARCHAR(255) UNIQUE NOT NULL,
+    FOREIGN KEY (id_candidato) REFERENCES cadastro_candidatos(id)
+);
