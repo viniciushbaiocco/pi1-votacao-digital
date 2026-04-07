21da0f1a2a211f5d9dd64bf268bb1ee7e1fd7807
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-04-07 20:22:51 -0300

    Revert "Adicionando o módulo GerenciadorDeEntrada.py para melhor organização do código e refatorando pequenas parte do menu_completo."
    This reverts commit 1544823ad262b6939917f0dc3ebbb7b17cc61549.

--- MenuCompleto.py
+++ MenuCompleto.py
@@ -1,5 +1,28 @@
-import SubMenus as sm
-import GerenciadorDeEntrada as ge
+import SubMenus
+
+def obter_entrada_inteira_valida(mensagem, min_val, max_val):
+    """
+    Solicita uma entrada inteira ao usuário e valida se está dentro de um intervalo.
+    Continua pedindo até que uma entrada válida seja fornecida.
+
+    Args:
+        mensagem(str): A mensagem que deve ser inserida após a apresentação do menu.
+        min_val(int): Valor mínimo para determinada escolha do menu.
+        max_val(int): Valor máximo para determinada escolha do menu.
+
+    Returns:
+        int: Retorna o valor inteiro (opção) escolhida pelo usuário.
+
+    """
+    while 1: # Substitui while True
+        try:
+            escolha = int(input("\n"+mensagem))
+            if min_val <= escolha <= max_val:
+                return escolha
+            else:
+                print(f"Opção inválida. Por favor, escolha uma opção entre {min_val} e {max_val}.")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro.")
 
 def menu_completo():
     """
@@ -12,21 +35,21 @@ def menu_completo():
     Returns:
         None
     """
-    executando_menu = 0 # Substitui o True
-    while (executando_menu == 0):
+    executando_menu = 1 # Substitui o True
+    while executando_menu:
 
-        sm.exibir_menu_principal()
-        escolha = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 3)
+        SubMenus.exibir_menu_principal()
+        escolha = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 3)
 
         match (escolha):
             case 1:  # Entra na parte de gerenciamento
-                sm.exibir_menu_gerenciamento()
-                escolha_gerenciamento = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
+                SubMenus.exibir_menu_gerenciamento()
+                escolha_gerenciamento = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
 
                 match (escolha_gerenciamento):
                     case 1:
-                        sm.exibir_menu_eleitores()
-                        escolha_eleitor = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 5)
+                        SubMenus.exibir_menu_eleitores()
+                        escolha_eleitor = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 5)
 
                         match escolha_eleitor:
                             case 1:
@@ -41,8 +64,8 @@ def menu_completo():
                                 print("Em desenvolvimento...")
 
                     case 2:
-                        sm.exibir_menu_candidatos()
-                        escolha_candidatao = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 5)
+                        SubMenus.exibir_menu_candidatos()
+                        escolha_candidatao = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 5)
 
                         match escolha_candidatao:
                             case 1:
@@ -59,13 +82,13 @@ def menu_completo():
                 # Após a execução do sub-menu, o loop principal continua e exibe o menu principal novamente
 
             case 2:  # Entra na parte de votação
-                sm.exibir_menu_votacao()
-                escolha_votacao = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
+                SubMenus.exibir_menu_votacao()
+                escolha_votacao = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
 
                 match (escolha_votacao):
                     case 1:
-                        sm.exibir_menu_sistema_votacao()
-                        escolha_sistema_votacao = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
+                        SubMenus.exibir_menu_sistema_votacao()
+                        escolha_sistema_votacao = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 2)
 
                         match escolha_sistema_votacao:
                             case 1:
@@ -74,8 +97,8 @@ def menu_completo():
                                 print("Em desenvolvimento...")
 
                     case 2:
-                        sm.exibir_menu_restultados_votacao()
-                        escolha_resultado_votacao = ge.obter_entrada_inteira_valida("Escolha uma opção: ", 1, 4)
+                        SubMenus.exibir_menu_restultados_votacao()
+                        escolha_resultado_votacao = obter_entrada_inteira_valida("Escolha uma opção: ", 1, 4)
 
                         match escolha_resultado_votacao:
                             case 1:
@@ -91,5 +114,5 @@ def menu_completo():
 
             case 3:
                 print("Saindo...")
-                executando_menu = 1
+                executando_menu = 0
 menu_completo()
