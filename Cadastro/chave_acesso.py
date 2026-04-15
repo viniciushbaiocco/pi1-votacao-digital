316c762b0b50c4a92f094d344beca2dddca7744b
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-04-15 14:43:40 -0300

    fix: Removendo a parte de testes do bancos de dados do arquivo chave_acesso.py

--- Cadastro/chave_acesso.py
+++ Cadastro/chave_acesso.py
@@ -2,8 +2,6 @@ from conexao_banco import  conexao_banco
 import random
 
 def geracao_chave_acesso(nome):
-    conexao = conexao_banco()
-    cursor = conexao.cursor()
 
     numero = random.randint(1000, 9999)
 
@@ -17,26 +15,5 @@ def geracao_chave_acesso(nome):
     chave_acesso = iniciais_nome + inicial_sobrenome + str(numero)
 
     print(f"Chave de acesso {chave_acesso} criada para o eleitor {nome}")
-
-    """
-    Teste para ver se a chave estava indo para o banco
-    sql_formula = 'UPDATE cadastro_eleitores SET chave_acesso = %s WHERE id = %s'
-    try:
-        cursor = conexao.cursor()
-        cursor.execute(sql_formula, (chave_acesso, id))
-        conexao.commit()
-        print(f"Chave de acesso {chave_acesso} criada para o eleitor {nome}")
-    except Exception as e:
-        print(f"Erro ao criar chave de acesso: {e}")
-        conexao.rollback()
-    finally:
-        if ("cursor" in locals() and cursor):
-            cursor.close()
-        if (conexao):
-            conexao.close()
-    """
-
-
-geracao_chave_acesso("Jonas da Silva")
 
 
