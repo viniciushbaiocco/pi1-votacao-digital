7f9dea1b6f2c310848fa47d673912d3d69e2778d
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-04-22 18:19:19 -0300

    feat: listar eleitor antes e depois de editado, verificação CPF e titulo valido e função sem parametros

--- Gerenciamento/edicao_eleitores.py
+++ Gerenciamento/edicao_eleitores.py
@@ -1,34 +1,72 @@
-def edicao_eleitores(id_eleitor, novo_nome, novo_cpf, novo_titulo):
+def edicao_eleitores():
     import mysql.connector
     import conexao_banco as conect
 
     conexao = conect.conexao_banco()
     cursor = conexao.cursor()
 
+    #utilizar entre CPF e Título para encontrar o eleitor no BD
+    print('Qual método quer usar para fazer a edição: \n 1 - CPF \n 2 - Título de Eleitor')
+    opcao=int(input('Opção escolhida: '))
+
+    match opcao:
+        case 1:
+            busca = input('CPF: ')
+            cursor.execute('SELECT id FROM eleitores WHERE cpf = %s', (busca,))
+        case 2:
+            busca = input('Título de Eleitor: ')
+            cursor.execute('SELECT id FROM eleitores WHERE titulo_eleitor = %s', (busca,))
+        case _:
+            return 'Opção Inválida, escolha entre 1 e 2.'
+
+    #verificar a existência do eleitor no BD
+    eleitor = cursor.fetchone()
+    if eleitor is None:
+        return 'Eleitor não encontrado.'
+    id_eleitor = eleitor[0]
+
+    #listar o eleitor antes da edição
+    cursor.execute('SELECT * FROM eleitores WHERE id = %s', (id_eleitor,))
+    print('\n - Eleitor antes da edição - ')
+    print(cursor.fetchone())
+
+    #edição dos dados do eleitor
+    novo_nome = input('Digite o novo nome: ')
+    novo_cpf = input('Digite o novo CPF: ')
+    novo_titulo = input('Digite o novo Título de Eleitor: ')
+
+    #verificar se o CPF ou Título é valido 
+    if len(novo_cpf) != 11:
+        return 'CPF Inválido'
+    if len(novo_titulo) != 12:
+        return 'Título de Eleitor Inválido'
+
+    #verificar unicidade do CPF
     cursor.execute('SELECT id FROM eleitores WHERE cpf = %s AND id != %s', (novo_cpf, id_eleitor))
-    #verificação unicidade CPF
     if cursor.fetchone():
         return 'CPF já cadastrado.'
-    
-    cursor.execute('SELECT id FROM eleitores WHERE titulo_eleitor = %s AND id != %s ', (novo_titulo, id_eleitor))
-    #verificação unicidade TITULO
-    if cursor.fetchone():
-        return 'Título já cadastrado.'
 
-    #atualização banco de dados
-    cursor.execute('''
+    #verificar unicidade do Título de Eleitor
+    cursor.execute('SELECT id FROM eleitores WHERE titulo_eleitor = %s AND id != %s', (novo_titulo, id_eleitor))
+    if cursor.fetchone():
+        return 'Título de Eleitor já cadastrado.'
+
+    #atualizar BD
+    cursor.execute(''' 
         UPDATE eleitores SET nome = %s,
         cpf = %s, titulo_eleitor = %s WHERE id = %s
-        ''', (novo_nome, novo_cpf, novo_titulo, id_eleitor))
-    
+    ''', (novo_nome, novo_cpf, novo_titulo, id_eleitor))
     conexao.commit()
+
+    #listar o eleitor após edição
+    cursor.execute('SELECT * FROM eleitores WHERE id = %s', (id_eleitor,))
+    print('\n - Eleitor depois da edição - ')
+    print(cursor.fetchone())
+
     cursor.close()
     conexao.close()
     return 'Eleitor editado com sucesso.'
 
-
 #NEXT STEPS
 #tentar otimizar mais de algum jeito
-#perguntar se pode usar o %s
-#adicionar uma listagem do eleitor ja editado
-#testar no pc da puc
+#perguntar se precisar editar algo a mais alem do nome cpf e titulo
