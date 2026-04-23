dd9065ee0afe3c013866480b5611142094bdfb5f
Guilherme Silveira <guilherme.ls10@puccampinas.edu.br>
2026-04-23 19:09:15 -0300

    feat: ajustei a formatacao de saída das informacoes trazidas do banco. Mais "amigável" ao usuário.

--- Gerenciamento/busca_eleitores.py
+++ Gerenciamento/busca_eleitores.py
@@ -9,7 +9,7 @@ def busca_eleitor():
     print("\n--- 4 - Buscar Eleitores ---")
     print("\nOpção 1: Busca pelo CPF")
     print("\nOpção 2: Busca pelo Título de eleitor")
-    print("\nOpção 3: Voltar")
+    print("\nOpção 3: Voltar ao Menu de Gerenciamento de Eleitores")
 
     from Menu import gerenciador_de_entrada as ge
     from Menu import sub_menus as sb
@@ -24,36 +24,67 @@ def busca_eleitor():
     while opcao != 3:
 
         if opcao == 1:
-            # no banco, cpf é CHAR(11)
+
             cpf = str(input("\nDigite o número do CPF a ser consultado: "))
-            validacao_cpf = verificacao_cpf.validacao_de_cpf(cpf)
+            # validacao_cpf = verificacao_cpf.validacao_de_cpf(cpf)
             # criptografar o cpf digitado
             comando = (
-                "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE cpf = %s")
+                "SELECT nome, mesario, titulo_eleitor, status_votacao FROM eleitores WHERE cpf = %s")
             valores = (cpf, )
 
             cursor.execute(comando, valores)
             dados_eleitor = cursor.fetchone()
-            # alterar para return depois de código finalizado.
-            print("\n", dados_eleitor)
+
+            nome = dados_eleitor[0]
+            mesario = dados_eleitor[1]
+            if mesario == 0:
+                mesario = "Não"
+            else:
+                mesario = "Sim"
+            titulo = dados_eleitor[2]
+            status_votacao = dados_eleitor[3]
+            if status_votacao == 0:
+                status_votacao = "NÃO VOTOU"
+            else:
+                status_votacao = "VOTOU"
+            print(f"\nNome eleitor: {nome}"
+                  f"\nTítulo de eleitor: {titulo}"
+                  f"\nMesário: {mesario}"
+                  f"\nStatus votação: {status_votacao}")
 
         if opcao == 2:
             titulo_eleitor = str(
                 input("Digite o número do Título de eleitor a ser consultado: "))
             # validar o titulo digitado
             comando = (
-                "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE titulo_eleitor = %s")
+                "SELECT nome, mesario, titulo_eleitor, status_votacao FROM eleitores WHERE titulo_eleitor = %s")
             valores = (titulo_eleitor, )
 
             cursor.execute(comando, valores)
             dados_eleitor = cursor.fetchone()
-            # alterar para return depois de código finalizado.
-            print("\n", dados_eleitor)
+
+            nome = dados_eleitor[0]
+            mesario = dados_eleitor[1]
+            if mesario == 0:
+                mesario = "Não"
+            else:
+                mesario = "Sim"
+            titulo = dados_eleitor[2]
+            status_votacao = dados_eleitor[3]
+            if status_votacao == 0:
+                status_votacao = "NÃO VOTOU"
+            else:
+                status_votacao = "VOTOU"
+            print(f"\nNome eleitor: {nome}"
+                  f"\nTítulo de eleitor: {titulo}"
+                  f"\nMesário: {mesario}"
+                  f"\nStatus votação: {status_votacao}")
 
         print("\n--- Deseja buscar um novo eleitor? ---")
         print("\nOpção 1: Busca pelo CPF")
         print("\nOpção 2: Busca pelo Título de eleitor")
-        print("\nOpção 3: Voltar")
+        print("\nOpção 3: Voltar ao Menu de Gerenciamento de Eleitores")
+
         opcao = ge.obter_entrada_inteira_valida(
             "\nDigite uma opção: ", 1, 3)
 
@@ -62,6 +93,7 @@ def busca_eleitor():
 
     print("\n")
     sb.exibir_menu_eleitores()
+    print("\n")
 
 # fazer a busca dos inputs com seus correspondentes no banco de dados
 # retonar com nome eleitor
@@ -69,12 +101,16 @@ def busca_eleitor():
 
 if __name__ == "__main__":
     busca_eleitor()
+
+
 # usando esse if para poder rodar o teste dentro deste arquivo e nao na main.py
 
 # Next steps:
-# Proteger a devolucao caso a opcao digitada seja diferente de 1, 2 e 3 - funcao gerenciador de entradas
 # Proteger a devolucao caso cpf ou titulo de eleitor invalido
 # Proteger caso cpf ou titulo nao encontrado no banco
-# Ao exibir o resultado, voltar para o menu de busca, permitindo novas buscas ou voltar pra o menu eleitores - ok
 # incluir validacao de CPF e criptografia
-# ajustar saída: legível e amigável para o usuário
+# Se pesquisar pelo titulo, devolver CPF tipo xxxxxxxxx-08?
+
+# Proteger a devolucao caso a opcao digitada seja diferente de 1, 2 e 3 - funcao gerenciador de entradas
+# ajustar saída: legível e amigável para o usuário ok!
+# Ao exibir o resultado, voltar para o menu de busca, permitindo novas buscas ou voltar pra o menu eleitores - ok
