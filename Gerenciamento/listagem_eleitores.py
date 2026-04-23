24d5e5114738f048e14d500538eb8860e6da0e1f
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-04-23 09:40:22 -0300

    fix: corrigi listagem_eleitores

--- Gerenciamento/listagem_eleitores.py
+++ Gerenciamento/listagem_eleitores.py
@@ -6,15 +6,20 @@ def listagem_eleitores ():
     cursor = conexao.cursor()
 
     #deve listar tudo da tabela eleitores (nome, cpf, titulo, etc)
+    
     cursor.execute('SELECT * FROM eleitores')
-    eleitores = cursor.fetchall()
-    return eleitores
+    total_eleitores = cursor.fetchall()
+    
+    for eleitores in range (total_eleitores):
+        print(eleitores)
 
     cursor.close()
     conexao.close()
+
+    return eleitores
 
     #NEXT STEPS 
     #Ver se utilizar o try/except para caso não houver eleitores na tabela
     #Testar no MYSQL no pc da PUC 
     #Ao exibir resultado voltar para o menu de buscas ou menu eleitores
-    
+    #erro no codigo, tentar usar o for
