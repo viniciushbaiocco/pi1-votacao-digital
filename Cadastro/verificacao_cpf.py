042e609f1b546591af572e1d0932ffee9316b66c
ARTHUR DE SOUZA GIMENES ANTIQUEIRA <arthur.sga@puccampinas.edu.br>
2026-04-23 15:50:30 -0300

    feat, chore: adicionei maior proteção contra erros, e fiz a relação da função com o banco de dados para consultar se o cpf já é existente

--- Cadastro/verificacao_cpf.py
+++ Cadastro/verificacao_cpf.py
@@ -1,5 +1,7 @@
 #Estutura da verificação de cpf
 def validacao_de_cpf(cpf_digitado_usuario):
+    import mysql.connector
+    import conexao_banco
     """
     Valida matematicamente um CPF verificando seus dois dígitos verificadores.
 Args:
@@ -9,12 +11,24 @@ Returns:
 """
     #Verificação do (1º dígito verificador)
     arrumando = cpf_digitado_usuario.replace(".", "").replace("-", "")
+
     #Verificação de tamanho do CPF
     if len(arrumando) != 11:
+        print(f"Erro: O CPF deve conter 11 dígitos.")
         return False
-    #Verificação se têm números repitídos
+    
+    #Verificação se têm números repitidos
     if arrumando == arrumando [0] * len(arrumando):
+        print("Erro: CPF inválido (sequência de números repetidos).")
         return False
+    
+    #Verificação se tem letras
+    try:
+        separado = [int(i) for i in arrumando]
+    except ValueError:
+        print("Erro: O CPF deve conter apenas números.")
+        return False
+    
     separado = [int(i) for i in arrumando]
     lista_1_DV = [10, 9, 8, 7, 6, 5, 4, 3, 2]
 
@@ -28,6 +42,7 @@ Returns:
         digito_1_DV = 11 - resto_1_DV
     simples_1_DV = separado[:9]
     nova_lista_1_DV = simples_1_DV + [digito_1_DV]
+
     #Verificação do (2º dígito verificador)
     lista_2_DV = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
     soma_2_DV = 0
@@ -35,4 +50,25 @@ Returns:
             soma_2_DV += nova_lista_1_DV[i] * lista_2_DV[i]
     resto_2_DV = soma_2_DV % 11
     digito_2_DV = 0 if resto_2_DV < 2 else 11 - resto_2_DV
-    return digito_1_DV == separado[9] and digito_2_DV == separado[10]
+    if digito_1_DV != separado[9] or digito_2_DV != separado[10]:
+        print("Erro: Calculo de CPF inválido (dígitos verificadores não conferem).")
+        return False
+
+    #Verifica se o CPF já existe no banco
+    
+    conexao.conexao_banco()
+    cursor = conexao.cursor()
+    query = "SELECT COUNT(*) FROM eleitores WHERE cpf = %s"
+    cursor.execute(query,(arrumando,))
+    resultado = cursor.fetchone()
+
+    cursor.close()
+    conexao.close()
+
+    if resultado [0] > 0:
+            print("CPF já cadastrado.")
+            return False
+
+   
+
+    return True
