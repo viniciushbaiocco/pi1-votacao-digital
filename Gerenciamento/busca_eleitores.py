35649ae2b49ae07c5cc8724514a07b4c68239a3b
Guilherme Silveira <guilherme.ls10@puccampinas.edu.br>
2026-04-23 16:53:50 -0300

    feat: inseri um laço de repeticao para que as opcoes voltem a aparecer para o usuario depois de efetuada a busca

--- Gerenciamento/busca_eleitores.py
+++ Gerenciamento/busca_eleitores.py
@@ -5,6 +5,7 @@
 
 
 def busca_eleitor():
+
     print("\n--- 4 - Buscar Eleitores ---")
     print("\nOpção 1: Busca pelo CPF")
     print("\nOpção 2: Busca pelo Título de eleitor")
@@ -12,48 +13,58 @@ def busca_eleitor():
 
     from Menu import gerenciador_de_entrada as ge
     from Menu import sub_menus as sb
+    import verificacao_cpf
     import conexao_banco as conect
 
     conexao = conect.conexao_banco()
     cursor = conexao.cursor()
 
-    opcao = ge.obter_entrada_inteira_valida("Digite uma opção: ", 1, 3)
-    if opcao == 1:
-        # no banco, cpf é CHAR(11)
-        cpf = str(input("Digite o número do CPF a ser consultado: "))
-        # validar o cpf digitado
-        # criptografar o cpf digitado
-        comando = (
-            "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE cpf = %s")
-        valores = (cpf, )
+    opcao = ge.obter_entrada_inteira_valida("\nDigite uma opção: ", 1, 3)
 
-        cursor.execute(comando, valores)
-        dados_eleitor = cursor.fetchone()
-        # alterar para return depois de código finalizado.
-        print(dados_eleitor)
+    while opcao != 3:
 
-    if opcao == 2:
-        # preciso entender se input será str ou int
-        titulo_eleitor = str(
-            input("Digite o número do Título de eleitor a ser consultado: "))
-        # validar o titulo digitado
-        comando = (
-            "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE titulo_eleitor = %s")
-        valores = (titulo_eleitor, )
+        if opcao == 1:
+            # no banco, cpf é CHAR(11)
+            cpf = str(input("\nDigite o número do CPF a ser consultado: "))
+            validacao_cpf = verificacao_cpf.validacao_de_cpf(cpf)
+            # criptografar o cpf digitado
+            comando = (
+                "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE cpf = %s")
+            valores = (cpf, )
 
-        cursor.execute(comando, valores)
-        dados_eleitor = cursor.fetchone()
-        # alterar para return depois de código finalizado.
-        print(dados_eleitor)
+            cursor.execute(comando, valores)
+            dados_eleitor = cursor.fetchone()
+            # alterar para return depois de código finalizado.
+            print("\n", dados_eleitor)
 
-    else:
-        sb.exibir_menu_eleitores()
+        if opcao == 2:
+            titulo_eleitor = str(
+                input("Digite o número do Título de eleitor a ser consultado: "))
+            # validar o titulo digitado
+            comando = (
+                "SELECT nome, mesario, titulo_eleitor FROM cadastro_eleitores WHERE titulo_eleitor = %s")
+            valores = (titulo_eleitor, )
+
+            cursor.execute(comando, valores)
+            dados_eleitor = cursor.fetchone()
+            # alterar para return depois de código finalizado.
+            print("\n", dados_eleitor)
+
+        print("\n--- Deseja buscar um novo eleitor? ---")
+        print("\nOpção 1: Busca pelo CPF")
+        print("\nOpção 2: Busca pelo Título de eleitor")
+        print("\nOpção 3: Voltar")
+        opcao = ge.obter_entrada_inteira_valida(
+            "\nDigite uma opção: ", 1, 3)
 
     cursor.close()
     conexao.close()
 
-    # fazer a busca dos inputs com seus correspondentes no banco de dados
-    # retonar com nome eleitor
+    print("\n")
+    sb.exibir_menu_eleitores()
+
+# fazer a busca dos inputs com seus correspondentes no banco de dados
+# retonar com nome eleitor
 
 
 if __name__ == "__main__":
@@ -61,9 +72,9 @@ if __name__ == "__main__":
 # usando esse if para poder rodar o teste dentro deste arquivo e nao na main.py
 
 # Next steps:
-# Proteger a devolucao caso a opcao digitada seja diferente de 1, 2 e 3
+# Proteger a devolucao caso a opcao digitada seja diferente de 1, 2 e 3 - funcao gerenciador de entradas
 # Proteger a devolucao caso cpf ou titulo de eleitor invalido
 # Proteger caso cpf ou titulo nao encontrado no banco
-# Ao exibir o resultado, voltar para o menu de busca, permitindo novas buscas ou voltar pra o menu eleitores
+# Ao exibir o resultado, voltar para o menu de busca, permitindo novas buscas ou voltar pra o menu eleitores - ok
 # incluir validacao de CPF e criptografia
 # ajustar saída: legível e amigável para o usuário
