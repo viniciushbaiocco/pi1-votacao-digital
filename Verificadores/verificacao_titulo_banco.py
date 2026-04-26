ddafde973b16844470c566173433e9b538a775c3
ARTHUR DE SOUZA GIMENES ANTIQUEIRA <arthur.sga@puccampinas.edu.br>
2026-04-26 20:46:53 -0300

    feat: criando arquivo de validação de titulo de eleitor e de verificação do titulo no banco de dados

---
+++ Verificadores/verificacao_titulo_banco.py
@@ -0,0 +1,35 @@
+
+def verificar_titulo_de_eleitor_banco(titulo):
+    """
+    Solicita o título validado para consulta no banco de dados.
+
+    Args:
+        mensagem(str): entrar com um título validado.
+
+    Returns:
+        mensagem(str): Retorna se há eleitor cadastrado com o título no banco de dados ou não.
+
+    """
+    import conexao_banco
+    conexao = conexao_banco.conexao_banco()
+    cursor = conexao.cursor()
+    query = "SELECT COUNT(*) FROM eleitores WHERE titulo_eleitor = %s"
+    cursor.execute(query, (titulo, ))
+    resultado = cursor.fetchone()
+    if resultado[0] == 0:
+        resultado = "Sem eleitor cadastrado com este título"
+    else:
+        resultado = "Eleitor cadastrado com este título"
+
+    cursor.close()
+    conexao.close()
+
+    return resultado
+# entender com o grupo se deixa o return no resultado com 0/1 ou mensagem
+
+
+# Teste da funcao
+if __name__ == "__main__":
+    titulo_eleitor = str(input("Título de eleitor: "))
+    result = verificar_titulo_de_eleitor_banco(titulo_eleitor)
+    print(result)
