276a0df8fc8a2425271a98706a33395c6866ba0a
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-04-28 15:03:32 -0300

    fix: colocando um break ao final para voltar ao menu

--- Gerenciamento/remocao_eleitores.py
+++ Gerenciamento/remocao_eleitores.py
@@ -29,7 +29,6 @@ def remocao_eleitores():
     #enquanto a opção estiver fora do intervalo, continua pedindo um valor válido (min,max)
     opcao = ge.obter_entrada_inteira_valida("\nDigite uma opção: ", 1, 2)
 
-    #enquanto não for escolhido a opção voltar (3), a remoção continua disponível
     while opcao != 3:
 
         #MUDANÇA: inicializo eleitor=none no começo do loop para reaproveitar o mesmo bloco de exibição/confirmação fora do match (evita duplicar código nos dois cases)
@@ -67,6 +66,7 @@ def remocao_eleitores():
                 else:
                     print("\nTítulo de Eleitor inválido. Por favor, refaça sua busca!")
 
+
         #se eleitor encontrado, exibe os dados e confirma a remoção
         if eleitor is not None:
 
@@ -93,13 +93,10 @@ def remocao_eleitores():
                 print("\nEleitor removido com sucesso.")
             else:
                 print("\nRemoção cancelada pelo usuário.")
+        break
 
     cursor.close()
     conexao.close()
-#usando esse if para poder rodar o teste dentro deste arquivo e nao na main.py
-
-if __name__ == "__main__":
-    remocao_eleitores()
 
 #NEXT STEPS
 #substituir a verificação len() != 12 do título pela função validar_titulo quando ela for adicionada em Verificadores
