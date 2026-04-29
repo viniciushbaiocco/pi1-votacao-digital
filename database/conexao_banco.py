bf072701dc890bfb007a3242e3a0ecbe674d622c
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-04-29 13:45:59 -0300

    chore: Retirando o print de conexão bem sucedida

--- database/conexao_banco.py
+++ database/conexao_banco.py
@@ -17,10 +17,7 @@ def conexao_banco():
             password="sua_senha",
             database="seu_banco"
         )
-        print("Conexão bem sucedida!")
         return conexao
     except mysql.connector.Error as err:
         print(f"Erro de conexão: {err}")
         return None
-
-
