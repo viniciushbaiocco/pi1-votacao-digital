fd79d9c32b20fe6060008a9b88cfd7b8464f261a
Guilherme Silveira <guilherme.ls10@puccampinas.edu.br>
2026-04-29 13:03:15 -0300

    Chore: ajustando devolucao de resposta para 'Mesario'. De False/True para Sim/Não

--- Cadastro/cadastro_eleitores.py
+++ Cadastro/cadastro_eleitores.py
@@ -12,14 +12,13 @@ def cadastrar_eleitor():
     """
     Realiza o cadastro completo de um eleitor no sistema, validando nome,
     CPF e título de eleitor antes de inserir os dados no banco.
-    
+
     Args:
         None
     Return:
         bool: Retorna True se o cadastro válido e False caso contrário
 
     """
-
 
     nome_valido = False
     while not nome_valido:
@@ -94,8 +93,10 @@ def cadastrar_eleitor():
 
     if resposta in ("S", "SIM"):
         mesario = True
+        retorno_mesario = 'Sim'
     else:
         mesario = False
+        retorno_mesario = 'Não'
 
     chave_acesso_original = chave_acesso.geracao_chave_acesso(nome)
     chave_criptografada = cripto.criptografar_chave_acesso(
@@ -117,14 +118,19 @@ def cadastrar_eleitor():
     cursor.close()
     conexao.close()
 
-    print("Cadastro realizado com sucesso.")
-    print("Nome:", nome)
+    print("\n ***** Cadastro realizado com sucesso! *****")
+    print("\nNome:", nome)
     print("CPF:", cpf)
     print("Título:", titulo_eleitor)
-    print("Mesário", mesario)
+    print("Mesário:", retorno_mesario)
     print("Chave de acesso:", chave_acesso_original)
+    print("\n")
 
     return True
 
 # Ajustes:
 # acertar nome só com espaco
+
+
+if __name__ == "__main__":
+    cadastrar_eleitor()
