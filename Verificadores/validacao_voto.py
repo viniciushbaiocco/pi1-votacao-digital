744c54d4f61bf2816a3887eda89c1d554f7008d0
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-05-05 09:52:01 -0300

    refactor: transformei a validacao de voto em uma função separada

---
+++ Verificadores/validacao_voto.py
@@ -0,0 +1,13 @@
+from colorama import Fore,Style
+
+def validacao_voto():
+    executando_entrada = 0
+    while (executando_entrada == 0):
+        try:
+            voto = int(input('\n Digite o número eleitoral do candidato que deseja votar: '))
+            if voto <0:
+                print(f'{Fore.RED}{Style.BRIGHT}Erro: Opção inválida. Por favor, escolha uma opção positiva. {Style.RESET_ALL}')
+            else:
+                executando_entrada = 1
+        except ValueError:
+            print(f'{Fore.RED}{Style.BRIGHT}Erro: Entrada inválida. Por favor, digite um número inteiro.{Style.RESET_ALL}')
--- Votacao/sistema_voto.py
+++ Votacao/sistema_voto.py
@@ -24,21 +24,13 @@ def sistema_voto ():
     print(f' Total de Candidatos Cadastrados: {len(total_candidatos)}')
 
     #verificação pro voto 
-    executando_entrada = 0
-    while (executando_entrada == 0):
-        try:
-            voto = int(input('\n Digite o número eleitoral do candidato que deseja votar: '))
-            if voto <0:
-                print(f'{Fore.RED}{Style.BRIGHT}Erro: Opção inválida. Por favor, escolha uma opção positiva. {Style.RESET_ALL}')
-            else:
-                executando_entrada = 1
-        except ValueError:
-            print(f'{Fore.RED}{Style.BRIGHT}Erro: Entrada inválida. Por favor, digite um número inteiro.{Style.RESET_ALL}')
 
     #verificação do número eleitoral no banco de dados
     query = "SELECT COUNT(*) FROM candidatos WHERE numero_votacao = %s"
     cursor.execute(query, (voto, ))
     resultado = cursor.fetchone()
-    
+    if resultado == (0,):
+        print('Você digitou um candidato inexistente, caso erre novamente o voto será considerado nulo.')
+        voto
     cursor.close()
     conexao.close()
