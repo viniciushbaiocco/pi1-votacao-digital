bf79da5ad3f696d527605d75d00facc25a02ec5e
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-07 13:50:44 -0300

    feat: Criando a validação da chave de acesso

---
+++ Verificadores/validacao_chave_acesso.py
@@ -0,0 +1,38 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def validar_chave_acesso(chave_acesso):
+
+    """
+    Verifica se a chave de acesso fornecida pelo usuário segue o padrão esperado:
+    dois primeiros caracteres são letras e os quatro últimos são números, totalizando 6 dígitos.
+
+    Args:
+        chave_acesso (str): A chave de acesso a ser validada.
+
+    Returns:
+        bool: True se a chave de acesso estiver na formatação adequada, False caso contrário.
+              Em caso de formatação incorreta, mensagens de erro detalhadas são impressas no console.
+    """
+
+    # Verifica se a chave de acesso possui 6 dígitos
+    if len(chave_acesso) != 6:
+        print(Fore.RED + Style.BRIGHT + "Chave de Acesso INVÁLIDA")
+        print(Fore.YELLOW+ Style.BRIGHT + "Chave de Acesso deve conter 6 dígitos")
+        print(Fore.YELLOW + Style.BRIGHT + "Não utilize espaços ou pontuações")
+        return False
+
+    # Verifica se os dois primeiros caracteres são letras
+    if not chave_acesso[0:2].isalpha():
+        print(Fore.RED + Style.BRIGHT + "Chave de Acesso INVÁLIDA")
+        print(Fore.YELLOW + Style.BRIGHT + "Os dois primeiros dígitos devem ser LETRAS")
+        return False
+
+    # Verifica se os quatro últimos caracteres são números
+    if not chave_acesso[2:6].isdigit():
+        print(Fore.RED + Style.BRIGHT + "Chave de Acesso INVÁLIDA")
+        print(Fore.YELLOW + Style.BRIGHT + "Os quatro últimos dígitos devem ser NÚMEROS")
+        return False
+
+    return True
