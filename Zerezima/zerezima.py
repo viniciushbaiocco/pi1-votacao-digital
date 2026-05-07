b6d760a0fb1b13bde58df398152f06ab752f2e46
Guilherme Silveira <guilherme.ls10@puccampinas.edu.br>
2026-05-07 09:52:55 -0300

    refactor: alteracao do DELETE para TRUNCATE ao zerar votos.

--- Zerezima/zerezima.py
+++ Zerezima/zerezima.py
@@ -3,8 +3,8 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
-def zerezima():
 
+def zerezima():
     """Função de zerezima para zerar todos os votos registados na tabela de votos.
         Também atualiza o status de votação dos eleitores para 'False'(não votou).
 
@@ -21,16 +21,18 @@ def zerezima():
     cursor = conexao.cursor()
 
     # zerar votos da tabela candidatos
-    comando1 = ("DELETE FROM votos;")
+    comando1 = ("TRUNCATE votos;")
     cursor.execute(comando1)
     conexao.commit()
-    print(Fore.YELLOW + Style.BRIGHT + "\nEliminando todos os votos registrados na tabela 'Votos'...")
+    print(Fore.YELLOW + Style.BRIGHT +
+          "\nEliminando todos os votos registrados na tabela 'Votos'...")
 
     # atualizar votos eleitores para "false"
     comando2 = ("UPDATE eleitores SET status_votacao = 0;")
     cursor.execute(comando2)
     conexao.commit()
-    print(Fore.YELLOW + Style.BRIGHT + "\nAtualizando status de votação de eleitores para 'Não'...")
+    print(Fore.YELLOW + Style.BRIGHT +
+          "\nAtualizando status de votação de eleitores para 'Não'...")
 
     print(Fore.WHITE + Style.BRIGHT + "\nZerézima finalizada!")
 
@@ -39,15 +41,21 @@ def zerezima():
     cursor.execute(comando3)
     lista_candidatos_zerado = cursor.fetchall()
 
-    print(Fore.WHITE + Style.BRIGHT + "\n" + "="*10 + "LISTAGEM DE CANDIDATOS E VOTOS" + "="*10)
+    print(Fore.WHITE + Style.BRIGHT + "\n" + "="*10 +
+          "LISTAGEM DE CANDIDATOS E VOTOS" + "="*10)
     print(Fore.WHITE + Style.BRIGHT + "="*50)
-    print(Fore.WHITE + Style.BRIGHT + f"{'CANDIDATO':<20} | {'PARTIDO':<10} | {'VOTOS':<5}")
+    print(Fore.WHITE + Style.BRIGHT +
+          f"{'CANDIDATO':<20} | {'PARTIDO':<10} | {'VOTOS':<5}")
     print(Fore.WHITE + Style.BRIGHT + "-" * 50)
 
     for nome, partido, votos in lista_candidatos_zerado:
-        print(Fore.WHITE + Style.BRIGHT + f"{nome:<20} | {partido:<10} | {votos:<5}")
+        print(Fore.WHITE + Style.BRIGHT +
+              f"{nome:<20} | {partido:<10} | {votos:<5}")
 
     print(Fore.WHITE + Style.BRIGHT + "="*50)
 
     cursor.close()
     conexao.close()
+
+
+zerezima()
