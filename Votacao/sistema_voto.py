1a492a0ef256ed8e95c0d63d16108c97fc8190e6
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-05-08 16:50:18 -0300

    feat: adicionei a ocorrencia de voto duplo no sistema votacao e a parte inicial do voto nulo caso candidato inexistente for digitado duas vezes

--- Votacao/sistema_voto.py
+++ Votacao/sistema_voto.py
@@ -1,5 +1,7 @@
+import os
 from database import conexao_banco as conect
 from Ocorrencias import voto_computado
+from Ocorrencias import voto_duplo
 from Validadores import gerenciador_de_entrada as ge, validacao_voto as val_voto
 from criptografia import criptografia as crip
 from Verificadores import verificacao_cpf_votacao as ver_cpf_vot
@@ -32,45 +34,55 @@ def sistema_voto ():
     chave_acesso = crip.criptografar_chave_acesso(chave_acesso)
     ver_votou = ver_eleit_vot.verificacao_eleitor_voto(chave_acesso)
         
-    if ver_cpf_vot.verificar_cpf_voto(cpf_4) == (1,) and ver_chave.verificar_chave_acesso_banco(chave_acesso) == (1,) and ver_votou == (0,):
-        #listagem de todos os candidatos do banco de dados para o usuário escolher entre eles
-        print('\n --- Listagem de Candidatos ---')
-        for candidatos in (total_candidatos):
-            print('=' * 50)
-            print(f' Nome: {candidatos['nome']}')
-            print(f' Partido: {candidatos['partido']}')
-            print(f' Número Eleitoral: {candidatos['numero_votacao']}')
-        print('=' *50)
-        print(f' Total de Candidatos Cadastrados: {len(total_candidatos)}')
-
-        #verificação pro voto 
-        voto = val_voto.validacao_voto()
-
-        #verificação do número eleitoral no banco de dados
-        query = "SELECT COUNT(*) FROM candidatos WHERE numero_votacao = %s"
-        cursor.execute(query, (voto, ))
-        resultado = cursor.fetchone()
-        if resultado == (0,):
-            print('Você digitou um candidato inexistente, caso erre novamente o voto será considerado nulo.')
-            voto = val_voto.validacao_voto()
-        else:
-            #listar o candidato para confirmação do voto
-            cursor.execute('SELECT * FROM candidatos WHERE numero_votacao = %s', (voto,))
-            candidato = cursor.fetchone()
-            print('=' * 50)
-            print(f' Nome: {candidato['nome']}')
-            print(f' Partido: {candidato['partido']}')
-            print(f' Número Eleitoral: {candidato['numero_votacao']}')
+    if ver_cpf_vot.verificar_cpf_voto(cpf_4) == (1,) and ver_chave.verificar_chave_acesso_banco(chave_acesso) == (1,):
+        if ver_votou == (0,):
+            #listagem de todos os candidatos do banco de dados para o usuário escolher entre eles
+            print('\n --- Listagem de Candidatos ---')
+            for candidatos in (total_candidatos):
+                print('=' * 50)
+                print(f' Nome: {candidatos['nome']}')
+                print(f' Partido: {candidatos['partido']}')
+                print(f' Número Eleitoral: {candidatos['numero_votacao']}')
             print('=' *50)
-            opcao = ge.obter_entrada_inteira_valida('\nCerteza que deseja votar nesse candidato? \n 1 - Sim \n 2 - Não ', 1, 2)
-            match opcao:
-                case 1:
-                    print('Voto Computado!')
-                    voto_computado.voto_computado()
-    else:
-        print('\n Erro, eleitor não encontrado ou já votou.')
+            print(f' Total de Candidatos Cadastrados: {len(total_candidatos)}')
+
+            #verificação pro voto 
+            voto = val_voto.validacao_voto()
+
+            #verificação do número eleitoral no banco de dados
+            query = "SELECT COUNT(*) FROM candidatos WHERE numero_votacao = %s"
+            cursor.execute(query, (voto, ))
+            resultado = cursor.fetchone()
+            if resultado == (0,):
+                print('Você digitou um candidato inexistente, caso erre novamente o voto será considerado nulo.')
+            else:
+                #listar o candidato para confirmação do voto
+                cursor.execute('SELECT * FROM candidatos WHERE numero_votacao = %s', (voto,))
+                candidato = cursor.fetchone()
+                print('=' * 50)
+                print(f' Nome: {candidato['nome']}')
+                print(f' Partido: {candidato['partido']}')
+                print(f' Número Eleitoral: {candidato['numero_votacao']}')
+                print('=' *50)
+                opcao = ge.obter_entrada_inteira_valida('\nCerteza que deseja votar nesse candidato? \n 1 - Sim \n 2 - Não ', 1, 2)
+                match opcao:
+                    case 1:
+                        print('Voto Computado!')
+                        voto_computado.voto_computado()
+            voto = val_voto.validacao_voto()
+            if voto == (0,):
+                print('Você digitou um candidato inexistente novamente, o voto será considerado nulo.')
+                #adicionar futuramente o voto nulo
+                voto_computado.voto_computado()
+        else:
+            print('\nErro, tentativa de voto duplo.')
+            voto_duplo.ocorrencia_voto_duplo()
+
 
     cursor.close()
     conexao.close()
 
-sistema_voto()
+#NEXT STEPS
+#ver como irei fazer para computar o voto nulo (suposição: considerar a variavel voto = 0 - provavel numero do candidato nulo na tabela)
+#ver se preciso usar a função de acesso negado
+#implementar mais coisa quando o restante do grupo acabar a parte deles
