07fdac855f1063a3e3b788212e53e4c0d19d607e
Felipe Piva <felipe.ps5@puccampinas.edu.br>
2026-05-08 18:36:58 -0300

    fix: arrumei função sistema_voto

--- Votacao/sistema_voto.py
+++ Votacao/sistema_voto.py
@@ -25,9 +25,9 @@ def sistema_voto ():
     while val_cpf_vot.validar_cpf_voto(cpf_4) == False:
         cpf_4 = input('\n''Digite os 4 primeiros digitos de seu CPF novamente: ')
 
-    chave_acesso = input('\nDigite sua chave de acesso: ')
+    chave_acesso = input('\nDigite sua chave de acesso: ').upper()
     while val_chave.validar_chave_acesso(chave_acesso) == False:
-        chave_acesso = input('\nDigite sua chave de acesso novamente: ')
+        chave_acesso = input('\nDigite sua chave de acesso novamente: ').upper()
 
     #criptografar, encontrar no BD e verificar se ja votou 
     cpf_4 = crip.criptografar_cpf(cpf_4)
@@ -35,6 +35,7 @@ def sistema_voto ():
     ver_votou = ver_eleit_vot.verificacao_eleitor_voto(chave_acesso)
         
     if ver_cpf_vot.verificar_cpf_voto(cpf_4) == (1,) and ver_chave.verificar_chave_acesso_banco(chave_acesso) == (1,):
+        #iniciar processo de votação apenas se o eleitor não votou
         if ver_votou == (0,):
             #listagem de todos os candidatos do banco de dados para o usuário escolher entre eles
             print('\n --- Listagem de Candidatos ---')
@@ -69,11 +70,37 @@ def sistema_voto ():
                     case 1:
                         print('Voto Computado!')
                         voto_computado.voto_computado()
-            voto = val_voto.validacao_voto()
-            if voto == (0,):
-                print('Você digitou um candidato inexistente novamente, o voto será considerado nulo.')
-                #adicionar futuramente o voto nulo
-                voto_computado.voto_computado()
+                    case 2:
+                        print('Encerrando operação...')
+            #segunda tentativa de votação
+            if resultado == (0,):
+                voto = val_voto.validacao_voto()
+                query = "SELECT COUNT(*) FROM candidatos WHERE numero_votacao = %s"
+                cursor.execute(query, (voto, ))
+                resultado = cursor.fetchone()
+                #segunda tentativa falha, voto será nulo
+                if resultado == (0,):
+                    print('Você digitou um candidato inexistente novamente, o voto será considerado nulo.')
+                    #adicionar futuramente o voto nulo
+                    voto_computado.voto_computado()
+                #segunda tentativa sucesso
+                else:
+                    #listar o candidato para confirmação do voto
+                    cursor.execute('SELECT * FROM candidatos WHERE numero_votacao = %s', (voto,))
+                    candidato = cursor.fetchone()
+                    print('=' * 50)
+                    print(f' Nome: {candidato['nome']}')
+                    print(f' Partido: {candidato['partido']}')
+                    print(f' Número Eleitoral: {candidato['numero_votacao']}')
+                    print('=' *50)
+                    opcao = ge.obter_entrada_inteira_valida('\nCerteza que deseja votar nesse candidato? \n 1 - Sim \n 2 - Não ', 1, 2)
+                    match opcao:
+                        case 1:
+                            print('Voto Computado!')
+                            voto_computado.voto_computado()
+                        case 2:
+                            print('Encerrando operação...')
+        #encerrar processo caso eleitor ja tenha votado    
         else:
             print('\nErro, tentativa de voto duplo.')
             voto_duplo.ocorrencia_voto_duplo()
@@ -88,3 +115,4 @@ def sistema_voto ():
 #ver como irei fazer para computar o voto nulo (suposição: considerar a variavel voto = 0 - provavel numero do candidato nulo na tabela)
 #ver se preciso usar a função de acesso negado
 #implementar mais coisa quando o restante do grupo acabar a parte deles
+#otimizar essa logica de programação lixosa q eu tive
