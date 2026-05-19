4b37f5159cbd361963bb6a6626f8045a2059c1ed
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-05-18 21:40:48 -0300

    remocao do colorama, usando rich pra cores

--- Validadores/validacao_cpf_votacao.py
+++ Validadores/validacao_cpf_votacao.py
@@ -1,6 +1,8 @@
-from colorama import Fore, Style
+from rich.console import Console
 from Visual.visual import limpar_tela
 from time import sleep
+
+console = Console(highlight=False)
 
 def validar_cpf_voto(cpf):
     """
@@ -18,8 +20,8 @@ def validar_cpf_voto(cpf):
     cpf_arrumado = cpf.replace(".", "").replace("-", "").replace(" ", "")
 
     if len(cpf_arrumado) != 4:
-        print(Fore.RED + Style.BRIGHT + "\nCPF INVÁLIDO")
-        print(Fore.YELLOW+ Style.BRIGHT + "Para a votação digite apenas os quatro primeiros dígitos do seu CPF")
+        console.print("\nCPF INVÁLIDO", style="bold red")
+        console.print("Para a votação digite apenas os quatro primeiros dígitos do seu CPF", style="bold yellow")
         sleep(1.5)
         limpar_tela()
         return False
@@ -28,8 +30,8 @@ def validar_cpf_voto(cpf):
     # Verfica se o cpf só possui letras
     for i in cpf_arrumado:
         if i.isalpha():
-            print(Fore.RED + Style.BRIGHT + "\nCPF INVÁLIDO")
-            print(Fore.YELLOW + Style.BRIGHT + "Não digite letras no CPF")
+            console.print("\nCPF INVÁLIDO", style="bold red")
+            console.print("Não digite letras no CPF", style="bold yellow")
             sleep(1.5)
             limpar_tela()
             return False
