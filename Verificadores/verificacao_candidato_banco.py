3dd613134cebb7a95315e76c80d7bc50325790f8
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-20 21:58:05 -0300

    feat: Adicionando a verificação no banco para candidatos

---
+++ Verificadores/verificacao_candidato_banco.py
@@ -0,0 +1,53 @@
+from database import conexao_banco
+from rich.console import Console
+from Visual.visual import limpar_tela
+from time import sleep
+
+console = Console(highlight=False)
+
+def verificar_candidato(partido, numero_votacao):
+
+    """
+    Verifica a validade de um candidato antes de seu cadastro no banco de dados.
+    
+    Esta função realiza duas verificações principais:
+    1. Garante que não haja mais de um candidato associado ao mesmo partido.
+    2. Garante que o número de votação proposto ainda não esteja em uso.
+    
+    Args:
+        partido (str): O nome do partido do candidato a ser verificado.
+        numero_votacao (int): O número de votação do candidato a ser verificado.
+        
+    Returns:
+        bool: True se o candidato for válido para cadastro (partido e número de votação únicos), False caso contrário.
+    """
+
+    conexao = conexao_banco.conexao_banco()
+    cursor = conexao.cursor()
+
+    query_partido = "SELECT COUNT(*) FROM candidatos WHERE partido = %s"
+
+    cursor.execute(query_partido, (partido,))
+    resultado_partido = cursor.fetchone()
+
+    if resultado_partido == (1,):
+        console.print("Apenas um candidato por partido é permitido", style="bold yellow")
+        sleep(1.5)
+        limpar_tela()
+        return False
+
+    query_numero_votacao = "SELECT COUNT(*) FROM candidatos WHERE numero_votacao = %s"
+
+    cursor.execute(query_numero_votacao, (numero_votacao,))
+    resultado_votacao = cursor.fetchone()
+
+    if resultado_votacao == (1,):
+        console.print("Número de Votação já cadastrado", style="bold yellow")
+        sleep(1.5)
+        limpar_tela()
+        return False
+
+    cursor.close()
+    conexao.close()
+
+    return True
