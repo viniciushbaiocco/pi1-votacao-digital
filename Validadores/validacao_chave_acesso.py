cf0020f69f98a46b5b3c009ac04a20ca4ad84a30
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-22 17:11:09 -0300

    feat: Criando a função que transcreve os acentos

--- Validadores/validacao_chave_acesso.py
+++ Validadores/validacao_chave_acesso.py
@@ -4,6 +4,29 @@ from time import sleep
 
 console = Console(highlight=False)
 
+
+def remover_acentos(texto: str) -> str:
+    """
+    Remove os acentos de uma string substituindo caracteres acentuados
+    por suas versões equivalentes sem acento.
+
+    Args:
+        texto (str): A string original que contém os acentos.
+
+    Returns:
+        str: A string limpa, contendo apenas caracteres sem acento.
+    """
+
+    # Mapeamento de caracteres acentuados para caracteres correspondentes sem acento
+    com_acentos = "áàâãäéèêëíìîïóòôõöúùûüçÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÕÖÚÙÛÜÇ"
+    sem_acentos = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC"
+
+    # Cria a tabela de tradução interna do Python
+    tabela_traducao = str.maketrans(com_acentos, sem_acentos)
+
+    # Aplica a substituição na string e retorna o resultado
+    return texto.translate(tabela_traducao)
+
 def validar_chave_acesso(chave_acesso):
     """
     Verifica se a chave de acesso fornecida pelo usuário segue o padrão esperado:
@@ -17,8 +40,11 @@ def validar_chave_acesso(chave_acesso):
               Em caso de formatação incorreta, mensagens de erro detalhadas são impressas no console.
     """
 
+    # Retira os acentos
+    chave_acesso_valida = remover_acentos(chave_acesso)
+
     # Verifica se a chave de acesso possui 7 dígitos
-    if len(chave_acesso) != 7:
+    if len(chave_acesso_valida) != 7:
         console.print("\nChave de Acesso INVÁLIDA", style="bold red")
         console.print("Chave de Acesso deve conter 7 dígitos", style="bold yellow")
         console.print("Não utilize espaços ou pontuações", style="bold yellow")
@@ -27,7 +53,7 @@ def validar_chave_acesso(chave_acesso):
         return False
 
     # Verifica se os dois primeiros caracteres são letras
-    if not chave_acesso[0:3].isalpha():
+    if not chave_acesso_valida[0:3].isalpha():
         console.print("\nChave de Acesso INVÁLIDA", style="bold red")
         console.print("Os três primeiros dígitos devem ser LETRAS", style="bold yellow")
         sleep(1.5)
@@ -35,7 +61,7 @@ def validar_chave_acesso(chave_acesso):
         return False
 
     # Verifica se os quatro últimos caracteres são números
-    if not chave_acesso[3:7].isdigit():
+    if not chave_acesso_valida[3:7].isdigit():
         console.print("\nChave de Acesso INVÁLIDA", style="bold red")
         console.print("Os quatro últimos dígitos devem ser NÚMEROS", style="bold yellow")
         sleep(1.5)
