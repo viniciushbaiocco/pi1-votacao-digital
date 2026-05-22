317e03efbb7277b96b875ad109f09b4f24069088
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-22 17:21:38 -0300

    fix: Protegendo o cpf da votação contra símbolos

--- Validadores/validacao_cpf_votacao.py
+++ Validadores/validacao_cpf_votacao.py
@@ -19,6 +19,7 @@ def validar_cpf_voto(cpf):
 
     cpf_arrumado = cpf.replace(".", "").replace("-", "").replace(" ", "")
 
+
     if len(cpf_arrumado) != 4:
         console.print("\nCPF INVÁLIDO", style="bold red")
         console.print("Para a votação digite apenas os quatro primeiros dígitos do seu CPF", style="bold yellow")
@@ -26,14 +27,14 @@ def validar_cpf_voto(cpf):
         limpar_tela()
         return False
 
-
-    # Verfica se o cpf só possui letras
-    for i in cpf_arrumado:
-        if i.isalpha():
+    for caractere in cpf_arrumado:
+        if not caractere.isdigit():
             console.print("\nCPF INVÁLIDO", style="bold red")
-            console.print("Não digite letras no CPF", style="bold yellow")
+            console.print("O CPF deve conter apenas números. Não digite letras ou símbolos.", style="bold yellow")
             sleep(1.5)
             limpar_tela()
             return False
 
     return True
+
+print(validar_cpf_voto("123/"))
